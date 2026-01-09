2d4c9c7de9be76a5bf9ea6c565be612cfad86f7f
Dan Habot <dan.habot@gmail.com>
2026-01-09 01:16:05 -0500

    step running

---
+++ run.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func runStep(s *Step, db Database) {
+	cmd := exec.Command("sh", "-c", s.Task.Script.String)
+
+	input_file, err := os.CreateTemp("/tmp", "*")
+	if err != nil {
+		panic(err)
+	}
+	output_dir := os.TempDir()
+
+	input_file.Write(s.Object)
+
+	cmd.Env = append(os.Environ(),
+		fmt.Sprintf("INPUT_FILE=%s", input_file.Name()),
+		fmt.Sprintf("OUTPUT_DIR=%s", output_dir),
+	)
+
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		panic(err)
+	}
+
+	dirs, err := os.ReadDir(output_dir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range dirs {
+		if file.IsDir() {
+			continue
+		}
+
+		body, err := os.ReadFile(file.Name())
+		if err != nil {
+			panic(err)
+		}
+
+		err = db.InsertStep(Step{
+			TaskID:   s.TaskID,
+			Object:   body,
+			PrevStep: s,
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
