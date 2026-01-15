5decad60f5711ee2ae9fbf4f1b22ab1e47a84c74
Dan Habot <dan.habot@gmail.com>
2026-01-15 01:38:45 -0500

    new export scheme

--- export.go
+++ export.go
@@ -4,40 +4,69 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/danhab99/idk/chans"
 )
 
 var exportLogger = log.New(os.Stderr, "[EXPORT] ", log.Ldate|log.Ltime|log.Lmsgprefix)
 
-func exportResults(database Database, stepName string, mode string) {
-	exportLogger.Printf("Exporting %s for step '%s'", mode, stepName)
-
+func exportResults(database Database, stepName string, inputPath string) {
 	step, err := database.GetStepByName(stepName)
 	if err != nil {
 		panic(err)
 	}
+	if step == nil {
+		exportLogger.Printf("Step '%s' not found", stepName)
+		return
+	}
 
-	tasks := database.GetTasksForStep(step.ID)
-	exportLogger.Printf("Found %d tasks", len(tasks))
+	if inputPath == "" {
+		// No input path specified - list all input tasks for this step
+		exportLogger.Printf("Listing input tasks for step '%s'", stepName)
+		tasks := <-chans.Accumulate(database.GetTasksForStep(step.ID))
+		exportLogger.Printf("Found %d tasks", len(tasks))
 
-	for task := range tasks {
-		objectPath := database.GetObjectPath(task.ObjectHash)
-
-		if mode == "output" && task.InputTaskID != nil {
-			// Get the input task to show which specific input produced this output
-			inputTask, err := database.GetTask(*task.InputTaskID)
-			if err != nil {
-				exportLogger.Printf("Warning: Could not get input task: %v", err)
-				fmt.Println(objectPath)
-				continue
-			}
-
-			inputHash := inputTask.ObjectHash
-			if len(inputHash) > 16 {
-				inputHash = inputHash[:16] + "..."
-			}
-			fmt.Printf("%s\t(from input %s)\n", objectPath, inputHash)
-		} else {
+		for _, task := range tasks {
+			objectPath := database.GetObjectPath(task.ObjectHash)
 			fmt.Println(objectPath)
+		}
+	} else {
+		// Input path specified - find this task and list its outputs
+		exportLogger.Printf("Finding outputs for input path: %s", inputPath)
+
+		// Resolve the absolute path
+		absInputPath, err := filepath.Abs(inputPath)
+		if err != nil {
+			panic(err)
+		}
+
+		// Find the task with this object path
+		tasks := <-chans.Accumulate(database.GetTasksForStep(step.ID))
+		var matchedTask *Task
+		for _, task := range tasks {
+			objectPath := database.GetObjectPath(task.ObjectHash)
+			absObjectPath, _ := filepath.Abs(objectPath)
+			if absObjectPath == absInputPath {
+				matchedTask = &task
+				break
+			}
+		}
+
+		if matchedTask == nil {
+			exportLogger.Printf("No task found for input path: %s", inputPath)
+			return
+		}
+
+		exportLogger.Printf("Found task %d", matchedTask.ID)
+
+		// Get all outputs for this task
+		outputTasks := <-chans.Accumulate(database.GetNextTasks(matchedTask.ID))
+		exportLogger.Printf("Found %d outputs", len(outputTasks))
+
+		for _, outputTask := range outputTasks {
+			outputPath := database.GetObjectPath(outputTask.ObjectHash)
+			fmt.Println(outputPath)
 		}
 	}
 }
